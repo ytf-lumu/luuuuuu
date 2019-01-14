59d793afdfed8bc9cd77c5e3a054ad5ef87192c3
ytf-lumu <527236947@qq.com>
2019-01-14 16:05:07 +0800

    2019-1-14 add

---
+++ nicoclient/src/main/java/com/nico/nicoclient/NicoclientApplication.java
@@ -0,0 +1,14 @@
+package com.nico.nicoclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NicoclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NicoclientApplication.class, args);
+	}
+
+}
+
---
+++ nicoclient/src/main/java/com/nico/nicoclient/sys/Sysclient.java
@@ -0,0 +1,27 @@
+package com.nico.nicoclient.sys;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Sysclient {
+
+  public static void main(String[] args){
+
+      String str = "941228";
+      //String pattern = "^[0-9a-z-]{3,5}b$";
+      String pattern = "^[0-9a-z_-]{3,8}$";
+      Pattern r = Pattern.compile(pattern);
+      Matcher m = r.matcher(str);
+      System.out.println(m.matches());
+  }
+  /**
+   * @author Administrator
+   * @Date  2019/1/2 0002 16:14
+   * @Param
+   * @return
+   * @version 1.0.0
+   */
+  public void cc(String cc){
+
+  }
+}
